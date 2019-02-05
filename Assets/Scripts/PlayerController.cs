a646ab3ffe8068dc6027aafa337061a5d810e5d3
Tiers <baptiste.tiers@epitech.eu>
2019-02-05 02:37:05 -0800

    First commit
    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public GameObject audio;
+
+    private Rigidbody rb;
+    private int count;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setScoreText();
+        winText.text = "";
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            audio.GetComponent<AudioSource>().Play();
+            other.gameObject.SetActive(false);
+            ++count;
+            setScoreText();
+        }
+    }
+
+    private void setScoreText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count >= 10)
+            winText.text = "You win !!";
+    }
+}
